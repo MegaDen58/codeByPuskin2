a91beb5fa2d09e10d907d0159bb39a352663b565
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-05-19 19:21:59 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,65 +6,72 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        StringBuilder readText = new StringBuilder();
+        String line;
+        /*
+        Считываем стихотворение из текстового файла.
+        */
         try {
-            /*
-            Считываем стихотворение из текстового файла.
-             */
             BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("text.txt")));
-            String line;
-            StringBuilder readText = new StringBuilder();
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 readText.append(line).append(" ");
             }
-            /*
-            В полученном тексте верхние регистры преобразовываем в нижние.
-            Создаём символьный массив этого текста.
-             */
-            String lowerText = readText.toString().toLowerCase(Locale.ROOT);
-            char[] characterText = lowerText.toCharArray();
+        }
+        catch (IOException ex){
+        System.out.println(ex.getMessage());
+        }
 
-            StringBuilder result = new StringBuilder(); // Переменная, в которую буду помещаться значения, соответствующие номеру строки и номеру символа.
+        /*
+        В полученном тексте верхние регистры преобразовываем в нижние.
+        Создаём символьный массив этого текста.
+        */
+        String lowerText = readText.toString().toLowerCase(Locale.ROOT);
+        char[] characterText = lowerText.toCharArray();
 
-            /*
-            Считываем номера строк и номера символов и текстового файла.
-             */
+        StringBuilder result = new StringBuilder(); // Переменная, в которую буду помещаться значения, соответствующие номеру строки и номеру символа.
+
+        /*
+        Считываем номера строк и номера символов и текстового файла.
+        */
+
+        StringBuilder lineAndCharacterNumbers = new StringBuilder();
+        try {
             BufferedReader br1 = new BufferedReader(new FileReader("numbers.txt"));
-            StringBuilder lineAndCharacterNumbers = new StringBuilder();
             while ((line = br1.readLine()) != null) {
                 lineAndCharacterNumbers.append(line);
             }
-
-            /*
-            Создаём 2 регулярны выражения.
-            Первое регулярное выражения для извлечения цифр из скобочек.
-            Второе регулярное выражения для извлечения значения после ";".
-             */
-            String regularForFindNumbers = "\\d+(\s*);(\s*)\\d+";
-            String regularForNumberOfSymbol = ";\\d+";
-            Pattern patternForNumbers = Pattern.compile(regularForFindNumbers);
-            Pattern patternForSymbol = Pattern.compile(regularForNumberOfSymbol);
-            Matcher matcherForFindNumbers = patternForNumbers.matcher(lineAndCharacterNumbers.toString());
-            
-            /*
-            Находим номера строк и номера символов.
-            Извлекаем второе число, вычитаем из него единицу и заносим значение в переменную index.
-            По индексу ищем соответствующую букву из текста. 
-            К переменной result добавляем найденную букву.
-             */
-            while(matcherForFindNumbers.find()){
-                String t = matcherForFindNumbers.group().replaceAll(" ", "");
-                Matcher matcherForSymbolNumber = patternForSymbol.matcher(t);
-                while(matcherForSymbolNumber.find()){
-                    String numberOfSymbol = matcherForSymbolNumber.group().replaceAll(";", "");
-                    int index = Integer.parseInt(numberOfSymbol) - 1;
-                    result.append(characterText[index]);
-                }
-            }
-            
-            System.out.println(result); // Выводим полученное слово.
         }
-        catch (IOException ex){
+        catch(IOException ex){
             System.out.println(ex.getMessage());
         }
+
+        /*
+        Создаём 2 регулярны выражения.
+        Первое регулярное выражения для извлечения цифр из скобочек.
+        Второе регулярное выражения для извлечения значения после ";".
+        */
+        String regularForFindNumbers = "\\d+(\s*);(\s*)\\d+";
+        String regularForNumberOfSymbol = ";\\d+";
+        Pattern patternForNumbers = Pattern.compile(regularForFindNumbers);
+        Pattern patternForSymbol = Pattern.compile(regularForNumberOfSymbol);
+        Matcher matcherForFindNumbers = patternForNumbers.matcher(lineAndCharacterNumbers.toString());
+            
+        /*
+        Находим номера строк и номера символов.
+        Извлекаем второе число, вычитаем из него единицу и заносим значение в переменную index.
+        По индексу ищем соответствующую букву из текста.
+        К переменной result добавляем найденную букву.
+        */
+        while(matcherForFindNumbers.find()){
+            String t = matcherForFindNumbers.group().replaceAll(" ", "");
+            Matcher matcherForSymbolNumber = patternForSymbol.matcher(t);
+            while(matcherForSymbolNumber.find()){
+                String numberOfSymbol = matcherForSymbolNumber.group().replaceAll(";", "");
+                int index = Integer.parseInt(numberOfSymbol) - 1;
+                result.append(characterText[index]);
+            }
+        }
+
+        System.out.println(result); // Выводим полученное слово.
     }
 }
